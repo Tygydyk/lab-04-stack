d72ba5ef65e9c87913b455fd8769157c9c4c5bce
Tygydyk <32385649+Tygydyk@users.noreply.github.com>
2019-01-16 16:54:11 +0300

    Update source.cpp

--- sources/source.cpp
+++ sources/source.cpp
@@ -1,3 +1,21 @@
-// Copyright 2018 Your Name <your_email>
+// Copyright 2018 Tygydyk
 
 #include <header.hpp>
+
+using namespace std;
+
+int main()
+{
+	Stack <int> *stack = new Stack<int>;
+	stack->push(1);
+	cout << stack->head() << endl;
+	int a = 2;
+	stack->push(a);
+	cout << stack->head() << endl;
+	stack->pop();
+	cout << stack->head() << endl;
+	stack->push_emplace(3, 4, 5);
+	cout << stack->head() << endl;
+
+	return 0;
+}
